86957658b0e9643ad71261f94223a34a2cb99ba4
InfiniteNel <juanrolagame@gmail.com>
2023-10-27 19:59:11 +0200

    Añadido navegación de SearchHero a DetailHero con datos usando safeargs

--- app/src/main/java/com/jroslar/heroapp/ui/searchhero/SearchHeroFragment.kt
+++ app/src/main/java/com/jroslar/heroapp/ui/searchhero/SearchHeroFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.jroslar.heroapp.databinding.FragmentSearchHeroBinding
 import com.jroslar.heroapp.ui.searchhero.adapter.SearchHeroAdapter
@@ -22,7 +22,7 @@ class SearchHeroFragment : Fragment() {
     private var _binding:FragmentSearchHeroBinding? = null
     private val binding get() = _binding!!
 
-    private val searchHeroViewModel by viewModels<SearchHeroViewModel>()
+    private val searchHeroViewModel:SearchHeroViewModel by viewModels()
 
     private lateinit var searchHeroadapter: SearchHeroAdapter
 
@@ -47,7 +47,9 @@ class SearchHeroFragment : Fragment() {
 
     private fun initAdapter() {
         searchHeroadapter = SearchHeroAdapter(onItemSelect = {
-            Toast.makeText(context, it.name, Toast.LENGTH_SHORT).show()
+            findNavController().navigate(
+                SearchHeroFragmentDirections.actionSearchHeroFragmentToDetailHeroFragment(it)
+            )
         })
 
         binding.rvSearchHeroList.apply {
