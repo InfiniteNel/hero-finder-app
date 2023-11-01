78b7af5725dcd1348b15ebdfddef76ed2ae1f701
InfiniteNel <juanrolagame@gmail.com>
2023-11-01 17:27:44 +0100

    Implementado dependencia Swipe Refresh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    //Swipe Refresh
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
