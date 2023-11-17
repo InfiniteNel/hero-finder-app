e1a1102d41eacf638517151783240b1460f9dd7b
InfiniteNel <juanrolagame@gmail.com>
2023-11-17 12:47:16 +0100

    Añadido SignupViewModel.

---
+++ app/src/main/java/com/jroslar/heroapp/ui/signup/SignupViewModel.kt
@@ -0,0 +1,8 @@
+package com.jroslar.heroapp.ui.signup
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SignupViewModel: ViewModel() {
+}
