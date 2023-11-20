4775ae95af10f576c83eeb280a0e388e9cbbe5d8
InfiniteNel <juanrolagame@gmail.com>
2023-11-20 12:23:06 +0100

    Añadido modelo de datos UserSignupData.

---
+++ app/src/main/java/com/jroslar/heroapp/ui/signup/model/UserSignupData.kt
@@ -0,0 +1,8 @@
+package com.jroslar.heroapp.ui.signup.model
+
+class UserSignupData (
+    val email: String,
+    val userName: String,
+    val password: String,
+    val repeatPassword: String
+)
