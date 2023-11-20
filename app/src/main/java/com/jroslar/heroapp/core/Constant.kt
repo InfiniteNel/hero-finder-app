ccfb93144a9759df2852a76a24121f607d8ee65d
InfiniteNel <juanrolagame@gmail.com>
2023-11-20 12:18:29 +0100

    Añadido constante "MIN_USERNAME_LENGTH".

--- app/src/main/java/com/jroslar/heroapp/core/Constant.kt
+++ app/src/main/java/com/jroslar/heroapp/core/Constant.kt
@@ -3,5 +3,6 @@ package com.jroslar.heroapp.core
 object Constant {
     const val MAX_NUMBER_HERO = 730
     const val MIN_PASSWORD_LENGTH = 6
+    const val MIN_USERNAME_LENGTH = 6
     const val WITHOUT_RESULT = "? ?"
 }
