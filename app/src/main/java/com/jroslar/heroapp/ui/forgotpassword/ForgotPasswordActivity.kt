af0023f65e74dad67240d2ec2b112bfd76fee7ae
InfiniteNel <juanrolagame@gmail.com>
2023-11-23 13:15:34 +0100

    Cambiado ForgotpasswordActivity. Añadido validar email.

--- app/src/main/java/com/jroslar/heroapp/ui/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/jroslar/heroapp/ui/forgotpassword/ForgotPasswordActivity.kt
@@ -1,10 +1,18 @@
 package com.jroslar.heroapp.ui.forgotpassword
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.jroslar.heroapp.R
+import com.jroslar.heroapp.core.extensions.loseFocusAfterAction
+import com.jroslar.heroapp.core.extensions.onTextChanged
 import com.jroslar.heroapp.databinding.ActivityForgotPasswordBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ForgotPasswordActivity : AppCompatActivity() {
@@ -23,6 +31,35 @@ class ForgotPasswordActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
+        initUIState()
+        initListener()
+    }
 
+    private fun initListener() {
+        binding.tietForgotpasswordEmail.loseFocusAfterAction(EditorInfo.IME_ACTION_DONE)
+        binding.tietForgotpasswordEmail.onTextChanged { onChangeField() }
+    }
+
+    private fun onChangeField() {
+        forgotPasswordViewmodel.onChangeText(
+            binding.tietForgotpasswordEmail.text.toString()
+        )
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                forgotPasswordViewmodel.state.collect {
+                    updateUI(it)
+                }
+            }
+        }
+    }
+
+    private fun updateUI(state: ForgotPasswordState) {
+        with(binding) {
+            tilForgotpasswordEmail.error =
+                if (state.isValidEmail) null else getString(R.string.ErrorEmail)
+        }
     }
 }
--- app/src/main/java/com/jroslar/heroapp/ui/forgotpassword/ForgotPasswordViewModel.kt
+++ app/src/main/java/com/jroslar/heroapp/ui/forgotpassword/ForgotPasswordViewModel.kt
@@ -1,5 +1,6 @@
 package com.jroslar.heroapp.ui.forgotpassword
 
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,4 +13,12 @@ class ForgotPasswordViewModel @Inject constructor() : ViewModel() {
     private var _state = MutableStateFlow(ForgotPasswordState())
     val state: StateFlow<ForgotPasswordState> = _state
 
+    fun onChangeText(email: String) {
+        _state.value = ForgotPasswordState(
+            isValidEmail = isValidEmail(email)
+        )
+    }
+
+    private fun isValidEmail(email: String) =
+        Patterns.EMAIL_ADDRESS.matcher(email).matches() && email.isNotEmpty()
 }
