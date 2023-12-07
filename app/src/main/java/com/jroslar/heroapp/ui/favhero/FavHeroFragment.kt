f025d9d6b5a09b82fc647ae8b392881e8d5f654a
InfiniteNel <juanrolagame@gmail.com>
2023-12-07 14:06:07 +0100

    Cambiado FavHeroFragment. Añadido control estados UI.

--- app/src/main/java/com/jroslar/heroapp/ui/favhero/FavHeroFragment.kt
+++ app/src/main/java/com/jroslar/heroapp/ui/favhero/FavHeroFragment.kt
@@ -6,8 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.jroslar.heroapp.R
 import com.jroslar.heroapp.databinding.FragmentFavHeroBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FavHeroFragment : Fragment() {
@@ -23,5 +28,38 @@ class FavHeroFragment : Fragment() {
     ): View {
         _binding = FragmentFavHeroBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                favHeroViewModel.state.collect {
+                    when(it) {
+                        FavHeroState.NoData -> noDataState()
+                        is FavHeroState.Success -> successState(it)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun successState(favHeroState: FavHeroState.Success) {
+        binding.tvUserEmail.text = favHeroState.email
+        binding.tvUserName.text = favHeroState.userName
+    }
+
+    private fun noDataState() {
+        binding.tvUserEmail.text = getString(R.string.tvFavHeroNoDataFound)
+        binding.tvUserName.text = getString(R.string.tvFavHeroNoDataFound)
     }
 }
